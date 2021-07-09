9867798af789473c1f312fbceebe72d6172bfa48
daydinn <daydinn@users.noreply.github.com>
2021-07-09 16:49:02 +0200

    neue infoseite
    Damit die alte menu2 Seite nicht leer steht

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AngeboteComponent } from './angebote.component';
-import { Menu2Component } from './menu2/menu2.component';
+import { InfopageComponent } from './delivers/infopage.component';
 import { RegistrierungComponent } from './registrierung/registrierung.component';
 import { AboutComponent } from './about/about.component';
 
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path:'angebote-component' , component:AngeboteComponent},
   
   {path : 'registrierung-component', component:RegistrierungComponent},
-  {path : 'menu2-component', component:Menu2Component},
+  {path : 'infopage-component', component:InfopageComponent},
   {path : 'about-component', component:AboutComponent },
  
  
