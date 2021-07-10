b12e762a5a7715e589d15139ecdad3a34aeb5e59
daydinn <daydinn@users.noreply.github.com>
2021-07-10 21:10:51 +0200

    erstmal die Farben ändern
    ░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄
    ░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄
    ░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█
    ░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░█
    ░▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░█
    █▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒█
    █▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█
    ░█▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█
    ░░█░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█
    ░░░█░░██░░▀█▄▄▄█▄▄█▄████░█
    ░░░░█░░░▀▀▄░█░░░█░███████░█
    ░░░░░▀▄░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█
    ░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░█
    ░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░█
    ░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░█

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AngeboteComponent } from './angebote.component';
 import { InfopageComponent } from './delivers/infopage.component';
 import { RegistrierungComponent } from './registrierung/registrierung.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   
-  { path:'angebote-component' , component:AngeboteComponent},
+  
   
   {path : 'registrierung-component', component:RegistrierungComponent},
   {path : 'infopage-component', component:InfopageComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import{HttpClientModule}from'@angular/common/http';
 import { ShopLiComponent } from './shop-li/shop-li.component';
-import { AngeboteComponent } from './angebote.component';
 import { HeaderComponent } from './header/header.component';
 import { RegistrierungComponent } from './registrierung/registrierung.component';
 import { ShopLiAddComponent } from './shop-li/shop-li-add.component';
 import { InfopageComponent } from './delivers/infopage.component';
 import { FooterComponent } from './footer/footer.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { FiltersComponent } from './shopping-cart/filters/filters.component';
+
 import { GetraenkListComponent } from './shopping-cart/getraenk-list/getraenk-list.component';
 import { CartComponent } from './shopping-cart/cart/cart.component';
 import { CartItemComponent } from './shopping-cart/cart/cart-item/cart-item.component';
@@ -31,14 +30,12 @@ import { FormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     ShopLiComponent,
-    AngeboteComponent,
     HeaderComponent,
     RegistrierungComponent,
     ShopLiAddComponent,
     InfopageComponent,
     FooterComponent,
     ShoppingCartComponent,
-    FiltersComponent,
     GetraenkListComponent,
     CartComponent,
     CartItemComponent,
