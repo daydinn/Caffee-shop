db67a34500a8d180dce12de3d32ab940f6228577
daydinn <daydinn@users.noreply.github.com>
2021-07-12 12:36:30 +0200

    navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   
-  public isCollapsed = true;
+  public isCollapsed = true;  //für Toggle button
 
   
   
